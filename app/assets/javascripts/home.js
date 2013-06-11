a77026e9ac2c23916df56340205777a94f6dff74
Michael Ibarra <bm2yogi@gmail.com>
2013-06-10 23:23:23 -0700

    more fun with angular.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,4 +2,16 @@ function HomeContrllr($scope){
     $scope.events = [
         {name:'Kickoff',host:'bm2yogi'},
         {name:'Barbecue',host:'phil'} ];
+
+    $scope.magicNumber = 4;
+
+    $scope.incr = function(){
+        console.log('incr');
+        $scope.magicNumber++;
+    }
+
+    $scope.decr = function(){
+        console.log('decr');
+        $scope.magicNumber--;
+    }
 }
